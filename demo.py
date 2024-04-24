ab5b5178f177d7aeb17834ef5ecc39cf0f03dd05
VictorIdowu <idsonvictor@gmail.com>
2024-04-24 11:40:03 +0100

    gridLayout

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QVBoxLayout,QHBoxLayout,QMainWindow,QWidget,QApplication,QLabel,QPushButton,QLineEdit,QCheckBox,QMessageBox
+from PyQt6.QtWidgets import QGridLayout,QVBoxLayout,QHBoxLayout,QMainWindow,QWidget,QApplication,QLabel,QPushButton,QLineEdit,QCheckBox,QMessageBox
 import sys
 from PyQt6.QtGui import QPixmap,QFont
 
@@ -11,24 +11,23 @@ class Window(QWidget):
     self.setWindowTitle("First PyQT Window")
     self.setGeometry(0, 0, 400, 150)
 
+    label_1 = QLabel("Label 1")
+    label_2 = QLabel("Label 2")
+    label_3 = QLabel("Label 3")
+
     button_1 = QPushButton("Button 1")
     button_2 = QPushButton("Button 2")
     button_3 = QPushButton("Button 3")
-    button_4 = QPushButton("Button 4")
 
-    h_box_1 = QHBoxLayout()
-    h_box_1.addWidget(button_1)
-    h_box_1.addWidget(button_2)
+    layout = QGridLayout()
+    self.setLayout(layout)
 
-    h_box_2 = QHBoxLayout()
-    h_box_2.addWidget(button_3)
-    h_box_2.addWidget(button_4)
-
-    v_box = QVBoxLayout()
-    v_box.addLayout(h_box_1)
-    v_box.addLayout(h_box_2)
-
-    self.setLayout(v_box)
+    layout.addWidget(label_1,0,0)
+    layout.addWidget(label_2,0,1)
+    layout.addWidget(label_3,0,2)
+    layout.addWidget(button_1,1,0)
+    layout.addWidget(button_2,1,1)
+    layout.addWidget(button_3,1,2)
 
 
 
